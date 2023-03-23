6c456a038b2f9eecceaa4b10bf3795de4412b5d8
ananyas0304 <ananyas0304@gmail.com>
2023-03-24 00:10:02 +0530

    8th commit

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import socket
 import pickle
 from _thread import *
-from sprites import *
 import sys
 
 
@@ -15,6 +14,8 @@ except socket.error as e:
 
 s.listen(2)
 print('Waiting for connections')
+
+other_players_list = []
 
 
 def thread_client(connection, player):
